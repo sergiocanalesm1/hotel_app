33aaa76f61db7520a7aece830525ed6a83ec1b5c
Simon <s.canales10@uniandes.edu.co>
2019-04-17 03:01:24 -0500

    LISTOOO

--- it1_A-03_s.canales_s.canales10/src/main/java/uniandes/isis2304/parranderos/negocio/HotelAndes.java
+++ it1_A-03_s.canales_s.canales10/src/main/java/uniandes/isis2304/parranderos/negocio/HotelAndes.java
@@ -478,9 +478,10 @@ public class HotelAndes
 		System.out.println("termino 2");
 		
 		//Alojamiento
+		int cantidadMantenimientoAlojamiento = 0;
 		for (int i = 0; i < habitaciones.length; i++) {
 			String numeroHabitacion = habitaciones[i];
-			long a = p.cancelarMantenimientoAlojamiento(numeroHabitacion);
+			p.cancelarMantenimientoAlojamiento(numeroHabitacion);
 		}
 		//Servicios
 		for (int j = 0; j < servicios.length; j++) {
