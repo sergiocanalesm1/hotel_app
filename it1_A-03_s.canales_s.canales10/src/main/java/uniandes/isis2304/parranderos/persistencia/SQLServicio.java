34f765500294aa32807e7b72c5358d1e19847563
wan <s.canales@uniandes.edu.co>
2019-04-17 18:23:18 -0500

    commitperro

--- it1_A-03_s.canales_s.canales10/src/main/java/uniandes/isis2304/parranderos/persistencia/SQLServicio.java
+++ it1_A-03_s.canales_s.canales10/src/main/java/uniandes/isis2304/parranderos/persistencia/SQLServicio.java
@@ -79,4 +79,10 @@ public class SQLServicio {
 		return (long)q.executeUnique();
 	}
 
+	public List<String> getAllServices(PersistenceManager pm) {
+		Query q = pm.newQuery(SQL, "SELECT id FROM " + pp.darTablaServicio());
+		q.setResultClass(Servicio.class);
+		return (List<String>) q.executeList();
+	}
+
 }
